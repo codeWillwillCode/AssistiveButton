f5092fc57028564c9ca8cfc19d9c43b28f0130a8
will <402876740@qq.com>
2015-07-12 18:35:30 +0800

    无bug

--- 手势/ViewController.m
+++ 手势/ViewController.m
@@ -25,7 +25,7 @@
     move.delegate = self;
     
     [self.botton addGestureRecognizer:move];
-    [self.botton addObserver:self forKeyPath:@"transform" options:NSKeyValueObservingOptionNew context:nil];
+//    [self.botton addObserver:self forKeyPath:@"transform" options:NSKeyValueObservingOptionNew context:nil];
     _move = move;
 
 }
@@ -45,17 +45,12 @@
     CGFloat buttonHeight = CGRectGetHeight(button.frame);
     CGFloat buttonTop    = CGRectGetMinY(button.frame);
     CGFloat buttonBottom = CGRectGetMaxY(button.frame);
+    CGFloat buttonOriX   = CGRectGetMinX(button.frame);
+    CGSize  buttonSize   = _botton.frame.size;
     
     CGFloat finalX;
     CGFloat finalY;
-    //上下边界处理
-    if (buttonTop < 0){
-        finalY = 0;
-    }else if (buttonBottom > screenBottom){
-        finalY = screenBottom - buttonHeight;
-    }else{
-        finalY = _botton.frame.origin.y;
-    }
+    BOOL    outSide = NO;
     
     if (buttonMidX < screenMidX) {
         finalX = 0;
@@ -63,12 +58,29 @@
         finalX = screenMaxX - buttonWidth;
     }
     
+    if (buttonBottom > screenBottom || buttonTop < 0) {
+        outSide = YES;
+    }
+    
+    //上下边界处理
+    if (buttonTop < 0){
+        finalY = 0;
+        finalX = buttonOriX;
+    }else if (buttonBottom >= screenBottom){
+        finalY = screenBottom - buttonHeight;
+        if (outSide)
+            finalX = buttonOriX;
+    }else{
+        finalY = buttonTop;
+    }
+    
     
     [UIView animateWithDuration:0.3 animations:^{
-        
-        _botton.frame = (CGRect){CGPointMake(finalX, finalY),_botton.frame.size};
+        _botton.frame = (CGRect){CGPointMake(finalX, finalY),buttonSize};
     }];
 }
+
+
 
 //移动控件
 - (void)moveBotton:(UIPanGestureRecognizer *)gesture
@@ -78,5 +90,56 @@
     _botton.transform = CGAffineTransformTranslate(_botton.transform, point.x, point.y);
     
     [gesture setTranslation:CGPointZero inView:_botton];
+    
+    if (_move.state == UIGestureRecognizerStateEnded)
+        [self dealWithEdgeEvent:_botton];
+    
+}
+
+- (void)dealWithEdgeEvent:(UIButton *)object{
+    UIButton *button = (UIButton *)object;
+    //按钮中点x
+    CGFloat buttonMidX   = CGRectGetMidX(button.frame);
+    CGFloat screenMidX   = CGRectGetMidX([UIScreen mainScreen].bounds);
+    CGFloat screenMaxX   = CGRectGetMaxX([UIScreen mainScreen].bounds);
+    CGFloat screenBottom = CGRectGetMaxY([UIScreen mainScreen].bounds);
+    CGFloat buttonWidth  = CGRectGetWidth(button.frame);
+    CGFloat buttonHeight = CGRectGetHeight(button.frame);
+    CGFloat buttonTop    = CGRectGetMinY(button.frame);
+    CGFloat buttonBottom = CGRectGetMaxY(button.frame);
+    CGFloat buttonOriX   = CGRectGetMinX(button.frame);
+    CGSize  buttonSize   = _botton.frame.size;
+    
+    CGFloat finalX;
+    CGFloat finalY;
+    BOOL    outSide = NO;
+    
+    if (buttonMidX < screenMidX) {
+        finalX = 0;
+    }else{
+        finalX = screenMaxX - buttonWidth;
+    }
+    
+    //y轴出界
+    if (buttonBottom > screenBottom || buttonTop < 0) {
+        outSide = YES;
+    }
+    
+    //上下边界处理
+    if (buttonTop < 0){
+        finalY = 0;
+        finalX = buttonOriX;
+    }else if (buttonBottom >= screenBottom){
+        finalY = screenBottom - buttonHeight;
+        if (outSide)
+            finalX = buttonOriX;
+    }else{
+        finalY = buttonTop;
+    }
+    
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        _botton.frame = (CGRect){CGPointMake(finalX, finalY),buttonSize};
+    }];
 }
 @end
