8eed1353c0d5ae5d78410f5d4a5b11f977057943
will <402876740@qq.com>
2015-07-23 14:41:15 +0800

    修复边角bug

--- 手势/ViewController.m
+++ 手势/ViewController.m
@@ -8,79 +8,33 @@
 
 #import "ViewController.h"
 
+
+#define ScreenBounds [UIScreen mainScreen].bounds
+
+
+
 @interface ViewController ()<UIGestureRecognizerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *botton;
 
 @property (strong ,nonatomic) UIPanGestureRecognizer *move;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
     UIPanGestureRecognizer *move = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(moveBotton:)];
     move.delegate = self;
-    
     [self.botton addGestureRecognizer:move];
+
 //    [self.botton addObserver:self forKeyPath:@"transform" options:NSKeyValueObservingOptionNew context:nil];
+
     _move = move;
 
 }
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
-
-    if (_move.state != UIGestureRecognizerStateEnded)
-        return;
-    
-    UIButton *button = (UIButton *)object;
-    //按钮中点x
-    CGFloat buttonMidX   = CGRectGetMidX(button.frame);
-    CGFloat screenMidX   = CGRectGetMidX([UIScreen mainScreen].bounds);
-    CGFloat screenMaxX   = CGRectGetMaxX([UIScreen mainScreen].bounds);
-    CGFloat screenBottom = CGRectGetMaxY([UIScreen mainScreen].bounds);
-    CGFloat buttonWidth  = CGRectGetWidth(button.frame);
-    CGFloat buttonHeight = CGRectGetHeight(button.frame);
-    CGFloat buttonTop    = CGRectGetMinY(button.frame);
-    CGFloat buttonBottom = CGRectGetMaxY(button.frame);
-    CGFloat buttonOriX   = CGRectGetMinX(button.frame);
-    CGSize  buttonSize   = _botton.frame.size;
-    
-    CGFloat finalX;
-    CGFloat finalY;
-    BOOL    outSide = NO;
-    
-    if (buttonMidX < screenMidX) {
-        finalX = 0;
-    }else{
-        finalX = screenMaxX - buttonWidth;
-    }
-    
-    if (buttonBottom > screenBottom || buttonTop < 0) {
-        outSide = YES;
-    }
-    
-    //上下边界处理
-    if (buttonTop < 0){
-        finalY = 0;
-        finalX = buttonOriX;
-    }else if (buttonBottom >= screenBottom){
-        finalY = screenBottom - buttonHeight;
-        if (outSide)
-            finalX = buttonOriX;
-    }else{
-        finalY = buttonTop;
-    }
-    
-    
-    [UIView animateWithDuration:0.3 animations:^{
-        _botton.frame = (CGRect){CGPointMake(finalX, finalY),buttonSize};
-    }];
-}
-
-
 
 //移动控件
 - (void)moveBotton:(UIPanGestureRecognizer *)gesture
@@ -90,53 +44,69 @@
     _botton.transform = CGAffineTransformTranslate(_botton.transform, point.x, point.y);
     
     [gesture setTranslation:CGPointZero inView:_botton];
-    
+
     if (_move.state == UIGestureRecognizerStateEnded)
         [self dealWithEdgeEvent:_botton];
-    
 }
 
-- (void)dealWithEdgeEvent:(UIButton *)object{
-    UIButton *button = (UIButton *)object;
-    //按钮中点x
+
+
+- (void)dealWithEdgeEvent:(UIButton *)button
+{
     CGFloat buttonMidX   = CGRectGetMidX(button.frame);
-    CGFloat screenMidX   = CGRectGetMidX([UIScreen mainScreen].bounds);
-    CGFloat screenMaxX   = CGRectGetMaxX([UIScreen mainScreen].bounds);
-    CGFloat screenBottom = CGRectGetMaxY([UIScreen mainScreen].bounds);
+    CGFloat buttonMinX   = CGRectGetMinX(button.frame);
+    CGFloat buttonMaxX   = CGRectGetMaxX(button.frame);
     CGFloat buttonWidth  = CGRectGetWidth(button.frame);
     CGFloat buttonHeight = CGRectGetHeight(button.frame);
     CGFloat buttonTop    = CGRectGetMinY(button.frame);
     CGFloat buttonBottom = CGRectGetMaxY(button.frame);
-    CGFloat buttonOriX   = CGRectGetMinX(button.frame);
     CGSize  buttonSize   = _botton.frame.size;
+    
+    CGFloat screenMaxX   = CGRectGetMaxX(ScreenBounds);
+    CGFloat screenBottom = CGRectGetMaxY(ScreenBounds);
+    CGFloat screenMidX   = CGRectGetMidX(ScreenBounds);
+    
+    CGFloat margin       = 30;
+    CGFloat screenMinX   = 0;
+    CGFloat screenTop    = 0;
     
     CGFloat finalX;
     CGFloat finalY;
-    BOOL    outSide = NO;
-    
-    if (buttonMidX < screenMidX) {
-        finalX = 0;
-    }else{
-        finalX = screenMaxX - buttonWidth;
-    }
+    BOOL    Y_outSide = NO;
+    BOOL    X_outSide = NO;
     
     //y轴出界
-    if (buttonBottom > screenBottom || buttonTop < 0) {
-        outSide = YES;
+    if (buttonBottom >= screenBottom || buttonTop <= screenTop) {
+        Y_outSide = YES;
     }
     
-    //上下边界处理
-    if (buttonTop < 0){
-        finalY = 0;
-        finalX = buttonOriX;
-    }else if (buttonBottom >= screenBottom){
-        finalY = screenBottom - buttonHeight;
-        if (outSide)
-            finalX = buttonOriX;
-    }else{
+    //x轴出界
+    if (buttonMinX <= screenMinX  || buttonMaxX >= screenMaxX) {
+        X_outSide = YES;
+    }
+    
+    //计算最终的X位置
+    if (Y_outSide && !X_outSide){
+        //Y出界，X不出界，X不变 （粘着上下）
+        finalX = buttonMinX;
+    }else{//Y出界X出界、Y不出界X不出界的情况，X粘着左右
+        if (buttonMidX < screenMidX) {
+            finalX = screenMinX;//X粘着左边
+        }else{
+            finalX = screenMaxX - buttonWidth;//X粘着右边
+        }
+    }
+    
+    //计算最终的Y位置
+    if (Y_outSide){//Y出界
+        if (buttonTop <= screenTop){
+            finalY = screenTop;
+        }else{
+            finalY = screenBottom - buttonHeight;
+        }
+    }else{//不出界
         finalY = buttonTop;
     }
-    
     
     [UIView animateWithDuration:0.3 animations:^{
         _botton.frame = (CGRect){CGPointMake(finalX, finalY),buttonSize};
